9e923cd7ef93bce3f6c896700f56463e69770393
Ashish Mishra <31382014+ashishobeystalent@users.noreply.github.com>
2020-09-06 02:43:07 +0530

    added files

---
+++ src/test/scala/com/ara/core/format/dataframe/transform/Transformation.scala
@@ -0,0 +1,36 @@
+package com.ara.core.format.dataframe.transform
+
+import java.sql.Date
+
+import scala.collection.JavaConverters._
+
+import org.apache.spark.sql.Row
+import com.ara.core.unittest.sharedcontext.SharedSparkSession
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.spark.sql.types.{DateType, IntegerType, StringType, StructField, StructType}
+import org.scalatest.FunSuite
+import com.ara.core.unittest.dataframe.DataFrameUtility.assertDataFrame
+
+class Transformation extends FunSuite with SharedSparkSession {
+
+  test("TransformationTest - Add Literal Column") {
+    val inputRDD = sc.parallelize(
+      List(
+        Row.fromTuple("AD234", "kiran", "sahiba", 20, Date.valueOf("2020-09-06"))
+      )
+    )
+
+    val inputDataFrameSchema = StructType(
+      Array(
+        StructField("id", StringType),
+        StructField("firstname", StringType),
+        StructField("lastname", StringType),
+        StructField("age", IntegerType),
+        StructField("dob", DateType)
+    ))
+    val inputDF = sqlContext.createDataFrame(inputRDD, inputDataFrameSchema)
+    val literalConfig = List.empty[Config]
+    val actualDF = Transformation().addConditionalLiteralColumnDataFrame(inputDF, literalConfig)
+    assertDataFrame(actualDF, inputDF)
+  }
+}
