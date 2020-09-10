5d02d73cdc0c2119c1e96cd361cbc7ac16162f3d
Ashish Mishra <31382014+ashishobeystalent@users.noreply.github.com>
2020-09-11 02:09:48 +0530

    added few functions

--- src/main/scala/com/ara/core/format/dataframe/transform/Transformation.scala
+++ src/main/scala/com/ara/core/format/dataframe/transform/Transformation.scala
@@ -3,8 +3,8 @@ package com.ara.core.format.dataframe.transform
 import scala.collection.JavaConverters._
 import org.apache.spark.sql.{Column, DataFrame}
 import com.typesafe.config.Config
-import org.apache.spark.sql.functions.{col, expr, when}
-import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.functions.{col, column, expr, lit, when}
+import org.apache.spark.sql.types.{StructField, StructType}
 
 class Transformation {
 
@@ -49,11 +49,118 @@ class Transformation {
       inputDF
     }
   }
-  def dropDuplicateColumnDataFrame(inputDF: DataFrame, dropDuplicateConfig: Config):DataFrame = ???
-  def addLiteralColumnDataFrame(inputDF: DataFrame, literalConfig: Config):DataFrame = ???
-  def castColumnDataFrame(inputDF: DataFrame, literalConfig: Config):DataFrame = ???
-  def castColumn(column: Column, colType: String, castList: List[String]):Column = ???
-  def columnRenameDataFrame(inputDF: DataFrame, columnRenameConfig: Config):DataFrame = ???
+
+  /**
+   *  drops the duplicate
+   * @param inputDF
+   * @param dropDuplicateConfig  { "columns": ["col1","col2","col3"]
+   *                             }
+   * @return
+   */
+  def dropDuplicateColumnDataFrame(inputDF: DataFrame, dropDuplicateConfig: Config):DataFrame = {
+  if(dropDuplicateConfig.isEmpty){ inputDF }
+  else {
+    val columnList = dropDuplicateConfig.getStringList("columns").asScala
+    if(columnList.isEmpty) { inputDF.dropDuplicates() }
+    else { inputDF.dropDuplicates(columnList) }
+  }
+  }
+
+  /**
+   *
+   * @param inputDF
+   * @param literalConfig Json of Type
+   *                      ''{
+   *                      "column1":"someLit",
+   *                      "column2":"someotherLit"
+   *                      }''
+   * @return DataFrame
+   */
+  def addLiteralColumnDataFrame(inputDF: DataFrame, literalConfig: Config):DataFrame = {
+    if(literalConfig.isEmpty){
+      inputDF
+    }
+    else {
+      val columnNameList = literalConfig.root().entrySet().asScala.map(_.getKey)
+      val outputDF = columnNameList.foldLeft(inputDF){(df,col)=>
+        df.withColumn(col, lit(literalConfig.getAnyRef(col)))
+      }
+      inputDF.sqlContext.createDataFrame(outputDF.rdd, StructType(outputDF.schema.map(_.copy(nullable = true))))
+    }
+  }
+
+  /**
+   * Casts the columns of the DataFrame according to the casting config provided
+   *
+   * @param inputDF
+   * @param castConfig {"column1": ["String","Date"], "column2": ["String","Int"] }
+   * @return
+   */
+  def castColumnDataFrame(inputDF: DataFrame, castConfig: Config):DataFrame = {
+    if  (castConfig.isEmpty){
+      inputDF
+    } else {
+      val columnNameList = castConfig.root().entrySet().asScala.map(_.getKey).toSeq
+      val projections = inputDF.dtypes.map{ case (colName, colType) =>
+        if(columnNameList.contains(colName)){
+          val castingOrder = castConfig.getStringList(colName).asScala.toList
+          val updatedCastingList = castingOrder.map(_.toLowerCase())
+          castColumn(inputDF(colName), colType, updatedCastingList).alias(colName)
+        }
+        else{
+          inputDF(colName)
+        }
+      }
+      inputDF.select(projections : _*)
+    }
+  }
+
+  /**
+   * Cast a column in the order of casting list and return a column
+   * @param column
+   * @param colType
+   * @param castList List["string", "double"]
+   * @return
+   */
+  def castColumn(column: Column, colType: String, castList: List[String]):Column = {
+    castList.foldLeft(column){ (col, castType)=>
+      if(colType == "StringType" && castType == "boolean") {
+        when(col === "true", true).otherwise(false)
+      } else {
+        col.cast(castType)
+      }
+    }
+
+  }
+
+  /**
+   * Rename dataFrame column names
+   *
+   * @param inputDF
+   * @param columnRenameConfig rename config of Json Type
+   *                           {
+   *                           col1: col2,
+   *                           col4: col6
+   *                           }
+   * @return
+   */
+  def columnRenameDataFrame(inputDF: DataFrame, columnRenameConfig: Config):DataFrame = {
+    val columnProjects = inputDF.schema.fields.map{ col =>
+      if(columnRenameConfig.hasPath(col.name)){
+        inputDF.col(col.name).alias(columnRenameConfig.getString(col.name))
+      }
+      else { inputDF.col(col.name) }
+    }
+    inputDF.select(columnProjects: _*)
+  }
+
+  /**
+   * Get Filter DataFrame based on Config
+   *
+   * @param inputDF
+   * @param filterConfigList
+   * @return
+   */
   def getFilteredDataFrame(inputDF: DataFrame, filterConfigList: List[Config]):DataFrame = ???
   def castValueDataType(columnStructField: StructField, value: String):DataFrame = ???
   def forkColumnDataFrame(inputDF: DataFrame, forkConfig: Config):DataFrame = ???
